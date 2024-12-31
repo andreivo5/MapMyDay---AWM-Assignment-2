0e109f6f2e8db4b09d80037bc37d651474e24654
andreivO5 <andreiv529@gmail.com>
2024-12-31 04:48:30 +0000

    added authentication (working)

--- mapmyday/mapmyday/settings.py
+++ mapmyday/mapmyday/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'map',
     'authentication',
+    'mapmyday',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'mapmyday.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mapmyday/templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
