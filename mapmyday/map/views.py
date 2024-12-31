59ddeaed135f1ce86c553db694fa91dec0da37b9
andreivO5 <andreiv529@gmail.com>
2024-12-31 19:33:46 +0000

    fixed map for main page

--- mapmyday/map/views.py
+++ mapmyday/map/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
-from .models import Location
+from django.shortcuts import render, redirect
+from django.contrib.gis.geos import Point
+from authentication.models import Profile
 
-# Create your views here.
 def map_view(request):
-    locations = Location.objects.all()
-    return render(request, 'map.html', {'locations': locations})
+    # checking if user is authenticated
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    try:
+        profile = Profile.objects.get(user=request.user)
+    except Profile.DoesNotExist:
+        return redirect('profile_setup')
+
+    # default location if the user's location is not set
+    if profile.location is None:
+        user_location = (53.3498, -6.2603)  # Dublin coordinates
+    else:
+        user_location = (profile.location.y, profile.location.x)
+
+    # passing user's location to the map template
+    return render(request, 'map.html', {
+        'user_latitude': user_location[0],
+        'user_longitude': user_location[1],
+    })
